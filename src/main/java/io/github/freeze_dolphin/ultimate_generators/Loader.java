e1e87a8b729af6772aae858f8aaaa4697c5e38d9
freeze-dolphin <freeze-dolphin@foxmail.com>
2020-11-08 14:43:23 +0800

    Save codes

---
+++ src/main/java/io/github/freeze_dolphin/ultimate_generators/Loader.java
@@ -0,0 +1,12 @@
+package io.github.freeze_dolphin.ultimate_generators;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Loader extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+}
