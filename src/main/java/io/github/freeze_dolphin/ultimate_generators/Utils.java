e50851e30219a42b0393be3513202f7566cb9c0a
freeze-dolphin <freeze-dolphin@foxmail.com>
2020-11-14 14:11:17 +0800

    Upload codes

---
+++ src/main/java/io/github/freeze_dolphin/ultimate_generators/Utils.java
@@ -0,0 +1,5 @@
+package io.github.freeze_dolphin.ultimate_generators;
+
+public class Utils {
+	
+}
---
+++ src/main/java/io/github/freeze_dolphin/ultimate_generators/objects/basics/UniversalMaterial.java
@@ -0,0 +1,17 @@
+package io.github.freeze_dolphin.ultimate_generators.objects.basics;
+
+import org.bukkit.Material;
+import org.bukkit.material.MaterialData;
+
+public class UniversalMaterial extends MaterialData {
+	
+	public UniversalMaterial(Material m) {
+		super(m);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public UniversalMaterial(Material m, int data) {
+		super(m, (byte) data);
+	}
+	
+}
