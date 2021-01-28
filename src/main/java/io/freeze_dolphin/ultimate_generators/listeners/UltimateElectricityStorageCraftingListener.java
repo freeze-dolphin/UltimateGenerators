bf19fc8bddbb4e93331a79b31ede3b9d4de70501
freeze-dolphin <40729044+freeze-dolphin@users.noreply.github.com>
2021-01-28 20:44:29 +0800

    .

--- src/main/java/io/freeze_dolphin/ultimate_generators/listeners/UltimateElectricityStorageCraftingListener.java
+++ src/main/java/io/freeze_dolphin/ultimate_generators/listeners/UltimateElectricityStorageCraftingListener.java
@@ -1,39 +1,41 @@
 package io.freeze_dolphin.ultimate_generators.listeners;
 
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.player.PlayerFishEvent.State;
 import org.bukkit.inventory.ItemStack;
 import io.freeze_dolphin.ultimate_generators.lists.UGItems;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Particles.FireworkShow;
 
 public class UltimateElectricityStorageCraftingListener implements Listener {
 
 	@EventHandler
-	public void afe(PlayerFishEvent e) {
-		if (e.isCancelled()) return;
-		if (e.getState().equals(State.CAUGHT_ENTITY)) {
-			if (e.getCaught() instanceof Item) {
-				Item ei = (Item) e.getCaught();
+	public void ede(EntityDamageEvent e) {
+		if (e.getCause().equals(DamageCause.LAVA)) {
+			if (e.getEntity() instanceof Item) {
+				Item ei = (Item) e.getEntity();
 				ItemStack lambda8 = UGItems.LAMBDA_ELECTRICITY_STORAGE.clone();
 				lambda8.setAmount(8);
 				if (lambda8.equals(ei.getItemStack())) {
-					// e.setCancelled(true);
-					Location el = ei.getLocation();
-					el.setY(el.getY() + 1);
+					e.setCancelled(true);
+					Location el = removeVector(ei.getLocation());
+					el.setY(el.getY());
 					ei.remove();
 
 					el.getWorld().playSound(el, Sound.UI_TOAST_CHALLENGE_COMPLETE, 1F, 1F);
-					Item dit = el.getWorld().dropItem(el, UGItems.KAPA_ELECTRICITY_STORAGE);
+					Item dit = el.getWorld().dropItemNaturally(el, UGItems.KAPA_ELECTRICITY_STORAGE);
 					el.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, el, 3);
-					dit.setGravity(false);
+					FireworkShow.launchFirework(el, Color.BLACK);
+					FireworkShow.launchFirework(el, Color.BLACK);
+					FireworkShow.launchFirework(el, Color.GRAY);
+					FireworkShow.launchFirework(el, Color.SILVER);
+					dit.setGlowing(true);
 					dit.setInvulnerable(true);
 					dit.setCustomNameVisible(true);
 					dit.setCustomName(dit.getItemStack().getItemMeta().getDisplayName());
@@ -41,30 +43,38 @@ public class UltimateElectricityStorageCraftingListener implements Listener {
 			}
 		}
 	}
-
+	
 	@EventHandler
-	public void ede(EntityDamageEvent e) {
-		if (e.getCause().equals(DamageCause.LAVA)) {
+	public void edep(EntityDamageEvent e) {
+		if (e.getCause().equals(DamageCause.LIGHTNING)) {
 			if (e.getEntity() instanceof Item) {
 				Item ei = (Item) e.getEntity();
 				ItemStack kapa8 = UGItems.KAPA_ELECTRICITY_STORAGE.clone();
 				kapa8.setAmount(8);
 				if (kapa8.equals(ei.getItemStack())) {
 					e.setCancelled(true);
-					Location el = ei.getLocation();
+					Location el = removeVector(ei.getLocation());
 					el.setY(el.getY() + 1);
 					ei.remove();
 
 					el.getWorld().playSound(el, Sound.UI_TOAST_CHALLENGE_COMPLETE, 1F, 1F);
-					Item dit = el.getWorld().dropItem(el, UGItems.PHI_ELECTRICITY_STORAGE);
+					Item dit = el.getWorld().dropItemNaturally(el, UGItems.PHI_ELECTRICITY_STORAGE);
 					el.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, el, 3);
-					dit.setGravity(false);
+					FireworkShow.launchFirework(el, Color.WHITE);
+					FireworkShow.launchFirework(el, Color.WHITE);
+					FireworkShow.launchFirework(el, Color.GRAY);
+					FireworkShow.launchFirework(el, Color.SILVER);
+					dit.setGlowing(true);
 					dit.setInvulnerable(true);
 					dit.setCustomNameVisible(true);
 					dit.setCustomName(dit.getItemStack().getItemMeta().getDisplayName());
 				}
 			}
 		}
+	}
+	
+	private Location removeVector(Location l) {
+		return new Location(l.getWorld(), l.getX(), l.getY(), l.getZ());
 	}
 
 }
