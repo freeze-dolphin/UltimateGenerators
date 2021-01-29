fb5f25b3e48dfa74ae9b6f81b0c3275ee31de5c0
freeze-dolphin <40729044+freeze-dolphin@users.noreply.github.com>
2021-01-29 16:08:11 +0800

    update building script

--- build.py
+++ build.py
@@ -1,21 +1,14 @@
 import os
 import sys
 
+#for comp in sys.argv: 
+#	print(comp)
+
+cmdC = " "
 for comp in sys.argv:
-	print(comp)
-
-argl = len(sys.argv)
-
-if (argl >= 1):
-	print("\nArguments are Detected")
-	cmdC = " "
-	for comp in sys.argv:
-		if (comp == sys.argv[0]):
-			continue
-		cmdC = cmdC + comp + " "
-	c = "mvn" + cmdC + "package"
-	print("\n[" + c + "]\n")
-	os.system(c)
-else: 
-	print("\nExecuting Pure Mode")
-	os.system("mvn package")
+	if (comp == sys.argv[0]):
+		continue
+	cmdC = cmdC + comp + " "
+c = "mvn" + cmdC + "package"
+print("\n[" + c + "]\n")
+os.system(c)
